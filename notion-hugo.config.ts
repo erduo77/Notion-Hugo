22e55fabd0f420666a94e22e215d4e0424f305a0
erduoya <144216104+erduoya77@users.noreply.github.com>
2024-11-22 13:43:12 +0800

    Update notion-hugo.config.ts
    Signed-off-by: erduoya <144216104+erduoya77@users.noreply.github.com>

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://erduoya.notion.site/1455bd7aa0b68018b66ed32ba9b3c0ad?v=1455bd7aa0b6812d9e08000ca6452a10&pvs=4',
+        page_url: 'https://erduoya.notion.site/Notion-DoIt-1465bd7aa0b680c8bdf2c762daa11513?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
